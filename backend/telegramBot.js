0f9671d5d21f6a51656bc5394b39152508c90283
den4ic0 <163349334+den4ic0@users.noreply.github.com>
2024-08-20 23:26:36 +0300

    improve code readability

--- backend/telegramBot.js
+++ backend/telegramBot.js
@@ -1,17 +1,20 @@
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.TELEGRAM_BOT_TOKEN;
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, { polling: true });
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
-  if (text === '/start') {
-    bot.sendMessage(chatId, 'Welcome to your bot. How may I assist you?');
-  } else if (text === '/notify') {
-    bot.sendMessage(chatId, 'Sending you a notification as requested.');
-  } else {
-    bot.sendMessage(chatId, `You said: ${text}`);
+  switch (text) {
+    case '/start':
+      bot.sendMessage(chatId, 'Welcome to your bot. How may I assist you?');
+      break;
+    case '/notify':
+      bot.sendMessage(chatId, 'Sending you a notification as requested.');
+      break;
+    default:
+      bot.sendMessage(chatId, `You said: ${text}`);
   }
 });
-bot.on("polling_error", console.log);
+bot.on("polling_error", (error) => console.log(error));
 module.exports = bot;
