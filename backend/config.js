44b463ffe9fb9600ec27d8f109a008f30aa5295d
den4ic0 <163349334+den4ic0@users.noreply.github.com>
2024-09-12 17:26:08 +0300

    implemented feature

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,49 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
+
 const mongoDB = process.env.MONGODB_URI;
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connection successful'))
-    .catch((err) => console.error('MongoDB connection error:', err));
-module.exports = mongoose.connection;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  autoReconnect: true,
+  reconnectTries: Number.MAX_VALUE,
+  reconnectInterval: 5000,
+};
+
+mongoose.connect(mongoDB, options).catch((err) => console.error('Initial MongoDB connection error:', err));
+
+const db = mongoose.connection;
+
+db.on('connecting', () => {
+  console.log('Attempting to connect to MongoDB...');
+});
+
+db.on('connected', () => {
+  console.log('MongoDB connection successful');
+});
+
+db.on('error', (err) => {
+  console.error('MongoDB connection error:', err);
+  mongoose.disconnect();
+});
+
+db.on('disconnected', () => {
+  console.log('MongoDB disconnected! Attempting to reconnect...');
+  mongoose.connect(mongoDB, options).catch((err) => console.error('MongoDB reconnection error:', err));
+});
+
+db.on('reconnected', () => {
+  console.log('MongoDB reconnected successfully');
+});
+
+let initialConnection = false;
+
+db.once('open', () => {
+  if (!initialConnection) {
+    console.log('MongoDB initial connection open');
+    initialConnection = true;
+  }
+});
+
+module.exports = db;
